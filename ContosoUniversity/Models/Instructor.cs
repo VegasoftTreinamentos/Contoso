de7960a30d9ce44c726a55c0077e16153e70a871
Donisetti Ferreira Cosma <donisetti@vegasoft.local>
2016-09-23 18:21:44 -0300

    Projeto publicacao inicial

---
+++ ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+   public class Instructor : Person
+   {
+      [DataType(DataType.Date)]
+      [Display(Name = "Admissão")]
+      public DateTime HireDate { get; set; }
+
+        [Display(Name = "Disciplinas")]
+        public virtual ICollection<Course> Courses { get; set; }
+        [Display(Name = "Sala")]
+        public virtual OfficeAssignment OfficeAssignment { get; set; }
+   }
+}
